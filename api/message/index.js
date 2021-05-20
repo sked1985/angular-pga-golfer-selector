a03ade3fa0eb6045920634481307b7e117baed94
Barry McGettigan <BMcGettigan@zarion.com>
2021-05-20 15:08:02 +0100

    Fixed API issue

--- api/message/index.js
+++ api/message/index.js
@@ -1,40 +1,43 @@
-var http = require("https");
+var http = require('https');
 
-module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-  var body = "";
-  var options = {
-    host: 'fly.sportsdata.io',
-    path: '/golf/v2/json/PlayerSeasonStats/2021',
-    method: 'GET',
-    headers: {
-      'Ocp-Apim-Subscription-Key': '9a223f53e5de442aa3aca629adae2c5e'
-    }
-  };
+module.exports = function (context, req) {
+    var body = "";
+    body += 'grant_type=' + req.query['grant_type'];
+    body += '&client_id=' + req.query['client_id'];
+    body += '&client_secret=' + req.query['client_secret'];
+    body += '&code=' + req.query['code'];
 
-  var response = '';
-  const request = http.request(options, (res) => {
-    context.log(`statusCode: ${res.statusCode}`);
+    var options = {
+      host: 'fly.sportsdata.io',
+      path: '/golf/v2/json/PlayerSeasonStats/2021',
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': '9a223f53e5de442aa3aca629adae2c5e'
+      }
+    };
 
-    res.on('data', (d) => {
-      response += d;
-    });
+    var response = '';
+    const request = http.request(options, (res) => {
+        context.log(`statusCode: ${res.statusCode}`)
 
-    res.on('end', (d) => {
-      context.res = {
-        body: response
-      };
-      context.log('In here with a result')
-      context.log(context.res.body)
-      context.done();
+        res.on('data', (d) => {
+            response += d;
+        })
+
+        res.on('end', (d) => {
+            context.res = {
+                body: response
+            }
+            context.done();
+        })
     })
-  })
 
-  request.on('error', (error) => {
-    context.log.error(error)
-    context.done();
-  });
+    request.on('error', (error) => {
+        context.log.error(error)
+        context.done();
+    })
 
-  request.write(body);
-  request.end();
+    request.write(body);
+    request.end();
 };
+
